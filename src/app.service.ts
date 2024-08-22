11f43544d710bc774c9fce131b1b26fb3922c350
adarista012 <ad.arista012@gmail.com>
2024-08-22 16:03:21 -0400

    env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Nest with mongodb!!';
+    return 'Hello Nest with mongodb and environment varibles!!!';
   }
 }
