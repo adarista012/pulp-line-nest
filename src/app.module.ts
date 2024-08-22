7725367183bf7dc739da9b318de8be8186c0aadc
adarista012 <ad.arista012@gmail.com>
2024-08-22 15:16:38 -0400

    mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionController } from './transaction/transaction.controller';
+import { TransactionService } from './transaction/transaction.service';
+import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    TransactionModule
+    ],
+  controllers: [AppController, TransactionController],
+  providers: [AppService, TransactionService],
 })
 export class AppModule {}
