9db01891311a95698f176af147d70eeba130922e
adarista012 <ad.arista012@gmail.com>
2024-08-22 15:47:19 -0400

    mongodb uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true
-    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     TransactionModule
     ],
